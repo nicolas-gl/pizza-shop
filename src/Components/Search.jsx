5cfc56def303d972def7a2adc78130ca506b7ec6
nicolas-gl <nicolas.glinkin@gmail.com>
2023-11-05 01:06:05 +0300

    seacrh block focus added when search text reset

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,25 +1,34 @@
 import styles from './Search.module.scss'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 export default function Search() {
 
+
   const [searchValue, setSearchValue] = useState('');
+  const inputRef = useRef();
+
   const onChangeSearchInput = (event) => {
     setSearchValue(event.target.value);
   };
 
+  const onReset = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
+
   return (
-    <div>
-
-      <div className={styles.searchBlock}>
-
-        {searchValue
-          ? <img width={16} height={16} onClick={() => setSearchValue('')} src="assets/img/icons/x.svg" alt="reset" />
-          : <img src="assets/img/icons/search.svg" alt="search" />
-        }
-
-        <input onChange={onChangeSearchInput} value={searchValue} type="text" placeholder="Find your Best..." />
-      </div>
+    <div className={styles.searchBlock}>
+      {searchValue
+        ? <img width={16} height={16} onClick={() => onReset()} src="assets/img/icons/x.svg" alt="reset" />
+        : <img src="assets/img/icons/search.svg" alt="search" />
+      }
+      <input
+        ref={inputRef}
+        id="searchInput"
+        onChange={onChangeSearchInput}
+        value={searchValue}
+        placeholder="Find your Best..." />
     </div>)
 }
